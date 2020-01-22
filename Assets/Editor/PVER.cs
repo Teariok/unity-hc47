c283eea3ba841b36c0f752a28da61d656e7341f0
Jay <teario@gmail.com>
2020-01-22 01:41:34 +0000

    Added PVER class with unpacking of the vertex data
    Vert data is unpacked into a Vec3 list. The offset map has a key of the start position and a value of the index in the list to start reading from.

---
+++ Assets/Editor/PVER.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PVER : Sector
+{
+    private Dictionary<uint, uint> m_KeyLookup;
+    private List<Vector3> m_Entries;
+
+    protected override void ReadBody()
+    {
+        if( HasMultidata )
+        {
+            base.ReadBody();
+        }
+        else
+        {
+            m_KeyLookup = new Dictionary<uint, uint>();
+            m_Entries = new List<Vector3>();
+
+            uint bodySize = SectorSize - BodySize;
+            long start = m_Data.Position;
+
+            while( m_Data.Position - start < bodySize )
+            {
+                uint index = (uint)(m_Data.Position - start);
+
+                float x = GetFloat();
+                float y = GetFloat();
+                float z = GetFloat();
+
+                m_Entries.Add( new Vector3(x,y,z) );
+                m_KeyLookup.Add( index, (uint)m_Entries.Count - 1 );
+                Debug.Log( new Vector3( x, y, z ) );
+            }
+        }
+    }
+
+    public Vector3[] GetVertices( uint index, int count )
+    {
+        if( m_KeyLookup.ContainsKey( index ) )
+        {
+            int start = (int)m_KeyLookup[index];
+            Vector3[] values = new Vector3[count];
+            m_Entries.CopyTo( start, values, 0, count );
+
+            return values;
+        }
+
+        return null;
+    }
+}
