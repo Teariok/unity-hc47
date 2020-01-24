497952e24cac65c4b7121bcb724067980b0e445a
Jay <teario@gmail.com>
2020-01-24 20:18:53 +0000

    Testing mesh generation

--- Assets/Editor/SPKProcessor.cs
+++ Assets/Editor/SPKProcessor.cs
@@ -46,23 +46,93 @@ public class SPKProcessor
 
         foreach( PROTEntry entry in collection )
         {
-            GameObject root = GameObject.CreatePrimitive( PrimitiveType.Sphere ); //new GameObject( entry.SectorId );
-            root.name = entry.SectorId;
-            
+            GameObject root = new GameObject( entry.SectorId );
+            root.transform.SetParent( parent.transform );
+
             PHEA.PHEAEntry? pheaData = phea.GetEntry( entry.PheaOffset );
             if( pheaData.HasValue )
             {
                 root.name = pnam.GetName( pheaData.Value.PnamIndex );
-                root.transform.position = ppos.GetPosition( pheaData.Value.PosIndex );
+                root.transform.localPosition = ppos.GetPosition( pheaData.Value.PosIndex );
+                
+                MeshFilter meshFilter = root.AddComponent<MeshFilter>();
+                meshFilter.mesh = GenerateMesh( rootSector, pheaData.Value );
+                root.AddComponent<MeshRenderer>();
             }
-
-            
-            root.transform.SetParent( parent.transform );
 
             if( entry.HasSubsectors )
             {
                 CreatePROTTree( rootSector, root, entry.Subsectors );
             }
         }
+    }
+
+    private static Mesh GenerateMesh( Sector rootSector, PHEA.PHEAEntry pheaData )
+    {
+        if( pheaData.TriangleCount == 0 && pheaData.QuadCount == 0 )
+        {
+            return null;
+        }
+
+        PVER pver = (PVER)rootSector.GetSubsector( "PVER" );
+        PFAC pfac = (PFAC)rootSector.GetSubsector( "PFAC" );
+
+        Mesh mesh = new Mesh();
+
+        List<Vector3> verts = new List<Vector3>();
+        List<int> indices = new List<int>();
+        int t = 0;
+
+        for( int i = 0; i < pheaData.QuadCount; i++ )
+        {
+            for( int j = 0; j < 4; j++ )
+            {
+                uint offs = (uint)(pheaData.QuadIndex + i * 4 + j);
+                ushort offsVal = pfac.GetIndex( offs );
+                ushort vertOP = (ushort)(offsVal * 3);
+                ushort vertDP = (ushort)(vertOP / 2);
+
+                float x = pver.GetFloat( (int)(pheaData.VertexIndex + vertDP) );
+                float y = pver.GetFloat( (int)(pheaData.VertexIndex + vertDP + 1) );
+                float z = pver.GetFloat( (int)(pheaData.VertexIndex + vertDP + 2) );
+
+                verts.Add( new Vector3( x, y, z ) );
+            }
+
+            indices.Add( t );
+            indices.Add( t + 1 );
+            indices.Add( t + 2 );
+
+            indices.Add( t );
+            indices.Add( t + 2 );
+            indices.Add( t + 3 );
+
+            t += 4;
+        }
+
+        int g = 0;
+        for( int i = 0; i < pheaData.TriangleCount; i++ )
+        {
+            for( int j = 0; j < 3; j++ )
+            {
+                uint offs = (uint)(pheaData.TriangleIndex + i * 3 + j);
+                ushort offsVal = pfac.GetIndex( offs );
+                ushort vertOP = (ushort)(offsVal * 3);
+                ushort vertDP = (ushort)(vertOP / 2);
+
+                float x = pver.GetFloat( (int)(pheaData.VertexIndex + vertDP) );
+                float y = pver.GetFloat( (int)(pheaData.VertexIndex + vertDP + 1) );
+                float z = pver.GetFloat( (int)(pheaData.VertexIndex + vertDP + 2) );
+
+                verts.Add( new Vector3( x, y, z ) );
+                indices.Add( t+g );
+                g++;
+            }
+        }
+
+        mesh.vertices = verts.ToArray();
+        mesh.triangles = indices.ToArray();
+
+        return mesh;
     }
 }
