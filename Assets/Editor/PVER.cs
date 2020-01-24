52b1bf017500ca49baea27be05ff7977857ca782
Jay <teario@gmail.com>
2020-01-24 20:17:48 +0000

    PVER changed to unpack data on demand
    Referenced against c47edit to unpack the data properly

--- Assets/Editor/PVER.cs
+++ Assets/Editor/PVER.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PVER : Sector
 {
     private Dictionary<uint, uint> m_KeyLookup;
-    private List<Vector3> m_Entries;
+    private List<float> m_Entries;
+    private byte[] m_RawData;
+    private List<Vector3> m_Verts;
 
     protected override void ReadBody()
     {
@@ -16,37 +18,50 @@ public class PVER : Sector
         else
         {
             m_KeyLookup = new Dictionary<uint, uint>();
-            m_Entries = new List<Vector3>();
+            m_Entries = new List<float>();
 
             uint bodySize = SectorSize - BodySize;
             long start = m_Data.Position;
 
+            //m_RawData = GetBytes( (int)bodySize );
+            m_Verts = new List<Vector3>();
             while( m_Data.Position - start < bodySize )
             {
-                uint index = (uint)(m_Data.Position - start);
+                m_RawData = GetBytes( (int)bodySize );
+                //m_Verts.Add( new Vector3( GetFloat(), GetFloat(), GetFloat() ) );
+                //uint index = (uint)(m_Data.Position - start);
 
-                float x = GetFloat();
-                float y = GetFloat();
-                float z = GetFloat();
-
-                m_Entries.Add( new Vector3(x,y,z) );
-                m_KeyLookup.Add( index, (uint)m_Entries.Count - 1 );
-                Debug.Log( new Vector3( x, y, z ) );
+                //m_Entries.Add( GetFloat() );
+                //m_KeyLookup.Add( index, (uint)m_Entries.Count - 1 );
             }
         }
     }
 
+    public float GetFloat( int index )
+    {
+        index *= 4;
+        byte[] data = new byte[] { m_RawData[index], m_RawData[index + 1], m_RawData[index + 2], m_RawData[index + 3] };
+        return System.BitConverter.ToSingle( data, 0 );
+    }
+    
     public Vector3[] GetVertices( uint index, int count )
     {
-        if( m_KeyLookup.ContainsKey( index ) )
+
+        //if( m_KeyLookup.ContainsKey( index ) )
         {
-            int start = (int)m_KeyLookup[index];
+            //int start = (int)m_KeyLookup[index];
+            int start = 0;
             Vector3[] values = new Vector3[count];
-            m_Entries.CopyTo( start, values, 0, count );
+
+            for( int i = 0; i < count; i++ )
+            {
+                values[i] = new Vector3( m_Entries[start], m_Entries[start+1], m_Entries[start+2]);
+                start += 6;
+            }
 
             return values;
         }
 
-        return null;
+        //return null;
     }
 }
