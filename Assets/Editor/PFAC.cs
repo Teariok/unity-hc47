5acc4feb0046acc57b1113a4b393080eb1dca6b2
Jay <teario@gmail.com>
2020-01-25 18:20:12 +0000

    Changed PFAC to process the data on load

--- Assets/Editor/PFAC.cs
+++ Assets/Editor/PFAC.cs
@@ -6,7 +6,6 @@ public class PFAC : Sector
 {
     private Dictionary<uint, int> m_KeyLookup;
     private List<ushort> m_Entries;
-    private byte[] m_RawData;
 
     protected override void ReadBody()
     {
@@ -22,9 +21,7 @@ public class PFAC : Sector
             uint bodySize = SectorSize - BodySize;
             long start = m_Data.Position;
 
-            m_RawData = GetBytes( (int)bodySize );
-
-            /*while( m_Data.Position - start < bodySize )
+            while( m_Data.Position - start < bodySize )
             {
                 uint index = (uint)(m_Data.Position - start);
 
@@ -32,32 +29,19 @@ public class PFAC : Sector
 
                 m_Entries.Add( value );
                 m_KeyLookup.Add( index, m_Entries.Count - 1 );
-            }*/
+            }
         }
     }
 
     public ushort GetIndex( uint index )
     {
         index *= 2;
-        byte[] data = new byte[] { m_RawData[index], m_RawData[index + 1] };
-        return System.BitConverter.ToUInt16( data, 0 );
-    }
-
-    public int[] GetTriangles( uint index, int count )
-    {
         if( m_KeyLookup.ContainsKey( index ) )
         {
-            int[] values = new int[count];
-            int start = m_KeyLookup[index];
-
-            for( uint i = 0; i < count; i++ )
-            {
-                values[i] = m_Entries[start + (int)i];
-            }
-
-            return values;
+            int i = m_KeyLookup[index];
+            return m_Entries[i];
         }
 
-        return null;
+        throw new System.Exception( string.Format( "Not Found: Face index at position {0} ({1})", index / 4, index ) );
     }
 }
